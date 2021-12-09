150654bec02c3baae809f34e1e4f1ecf64c7dda2
Devin MacArthur <jdmacar@pdx.edu>
2021-12-09 15:23:55 -0800

    commit 9

---
+++ ojd_evictions/run2021.py
@@ -0,0 +1,38 @@
+import os
+import psycopg2
+from psycopg2 import sql
+from datetime import date, timedelta
+import subprocess
+
+today = date.today()
+#os.getcwd()
+#os.makedirs("G:\\My Drive\\DAILY_SCRAPE_OJD\\Daily_Cases_" + today.strftime("%m_%d_%Y"))
+#os.makedirs("C:\\ojdevictions\\" + today + "\\2021_Court_Documents_" + today)
+
+#make sql database
+con = psycopg2.connect(database="postgres", user='postgres', password='admin', host='127.0.0.1', port= '5432')
+con.autocommit = True
+cur = con.cursor()
+name_Database =  "ojdevictions_2021_" + today.strftime("%Y%m%d")
+cur.execute("CREATE database "  + name_Database + ";")
+con.close()
+
+# call the crawl
+# os.system("scrapy crawl ojd_evictions")
+
+cmd = "scrapy"
+arg1 = "crawl"
+arg2 = "ojd-evictions-2021"
+subprocess.call([cmd, arg1, arg2], shell = True)
+
+# run r script to get tables and write to csv
+command = "C:\\Program Files\\R\\R-4.0.5\\bin\\Rscript.exe"
+path2script = "R_dataCleaning/flat_file_evictions.R"
+subprocess.call([command, path2script], shell=True)
+
+# get file extenstions
+# cmd = os.getcwd() + "\\trid_w32\\trid.exe"
+# path2 = "C:\\ojdevictions\\" + today + "\\2021_Court_Documents_" + today + "\\*"
+# arg1 = "-ae"
+#
+# subprocess.call([cmd, path2, arg1], shell=True)
