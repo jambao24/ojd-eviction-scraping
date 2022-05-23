9e72cc4d35c5d52e82f04e498f3f0db55f8603b6
James Bao <jambao@pdx.edu>
2022-05-22 19:55:04 -0700

    Updated FF-vs-OLC checking R script post 05/22 weekly scrape

--- R_dataCleaning/flatfile-OLC-checking_scratch.R
+++ R_dataCleaning/flatfile-OLC-checking_scratch.R
@@ -1,5 +1,5 @@
 # James Bao
-# 2022 May 13
+# 2022 May 22 version
 # scratch R code for analyzing and interpreting weekly scrape data
 
 library(readxl)
@@ -9,41 +9,41 @@ library(plyr)
 # for Goal 1, we want cols B (case_code), M, N, O, R, S
 # for Goal 2, we want cols B (case_code). L, Q, V, U
 # for Goal 3, we want cols B (case_code), H
-FF_0514 <- read.csv("G:/Shared drives/ojdevictions/ScrapeData/full_scrape_20220514/flat_file.csv")
-FF_0514_1 = FF_0514[,-1]
-FF_0514_1 <- subset(FF_0514_1, select = -c(case_name:status))
-FF_0514_1 <- subset(FF_0514_1, select = -c(plaintiff_name:Agent))
-FF_0514_1 <- subset(FF_0514_1, select = -c(tenant_lawyer:landlord_lawyer))
-FF_0514_1 <- subset(FF_0514_1, select = -c(landlord_has_lawyer:zip))
-#View(FF_0514_1)
+FF_0522 <- read.csv("G:/Shared drives/ojdevictions/ScrapeData/full_scrape_20220522/flat_file.csv")
+FF_0522_1 = FF_0522[,-1]
+FF_0522_1 <- subset(FF_0522_1, select = -c(case_name:status))
+FF_0522_1 <- subset(FF_0522_1, select = -c(plaintiff_name:Agent))
+FF_0522_1 <- subset(FF_0522_1, select = -c(tenant_lawyer:landlord_lawyer))
+FF_0522_1 <- subset(FF_0522_1, select = -c(landlord_has_lawyer:zip))
+#View(FF_0522_1)
 
-FF_0514_2 = FF_0514[,-1]
-FF_0514_2 <- subset(FF_0514_2, select = -c(case_name:defendant_addr))
-FF_0514_2 <- subset(FF_0514_2, select = -c(Judgment_General:tenant_lawyer))
-FF_0514_2 <- subset(FF_0514_2, select = -c(FTA:FTAFirst))
-FF_0514_2 <- subset(FF_0514_2, select = -c(FTAFirstXJudgmentGeneral:zip))
-#View(FF_0514_2)
+FF_0522_2 = FF_0522[,-1]
+FF_0522_2 <- subset(FF_0522_2, select = -c(case_name:defendant_addr))
+FF_0522_2 <- subset(FF_0522_2, select = -c(Judgment_General:tenant_lawyer))
+FF_0522_2 <- subset(FF_0522_2, select = -c(FTA:FTAFirst))
+FF_0522_2 <- subset(FF_0522_2, select = -c(FTAFirstXJudgmentGeneral:zip))
+#View(FF_0522_2)
 
-FF_0514_3 = FF_0514[,-1]
-FF_0514_3 <- subset(FF_0514_3, select = -c(case_name:status))
-FF_0514_3 <- subset(FF_0514_3, select = -c(plaintiff_name:zip))
-#View(FF_0514_3)
+FF_0522_3 = FF_0522[,-1]
+FF_0522_3 <- subset(FF_0522_3, select = -c(case_name:status))
+FF_0522_3 <- subset(FF_0522_3, select = -c(plaintiff_name:zip))
+#View(FF_0522_3)
 
 # for Goal 3, we want col C (case_code), F (decision), G (judgment date)
-judgments_0514 <- read.csv("G:/Shared drives/ojdevictions/ScrapeData/full_scrape_20220514/judgments.csv")
+judgments_0522 <- read.csv("G:/Shared drives/ojdevictions/ScrapeData/full_scrape_20220522/judgments.csv")
 # https://www.youtube.com/watch?v=6inAoQddaj0
 # convert date column to Date data type
-judgments_0514$date <- as.Date(judgments_0514$date, format = '%m/%d/%Y')
+judgments_0522$date <- as.Date(judgments_0522$date, format = '%m/%d/%Y')
 # remove unwanted rows
-judgments_0514 = judgments_0514[,-1]
-judgments_0514 = judgments_0514[,-1]
-judgments_0514 = judgments_0514[,-2]
-judgments_0514 = judgments_0514[,-2]
-judgments_0514 = judgments_0514[,-5]
-#View(judgments_0514)
+judgments_0522 = judgments_0522[,-1]
+judgments_0522 = judgments_0522[,-1]
+judgments_0522 = judgments_0522[,-2]
+judgments_0522 = judgments_0522[,-2]
+judgments_0522 = judgments_0522[,-5]
+#View(judgments_0522)
 # filter for rows that contain judgment dates within the current week (2022-05-08 through 2022-05-14)
-judgments_0514_curr = judgments_0514[judgments_0514$date > "2022-05-07" & judgments_0514$date < "2022-05-15", ]
-judgments_0514_curr <- judgments_0514_curr[complete.cases(judgments_0514_curr),]
+judgments_0522_curr = judgments_0522[judgments_0522$date > "2022-05-07" & judgments_0522$date < "2022-05-15", ]
+judgments_0522_curr <- judgments_0522_curr[complete.cases(judgments_0522_curr),]
 
 # for Goal 1, we want cols A (case_code), H, AA, AE
 # for Goal 2, we want cols A (case_code), W, V
@@ -66,9 +66,9 @@ OLC_updated_2 <- subset(OLC_updated_2, select = -c(SB278_setover:Notes))
 # https://rforjournalists.com/2018/04/10/sql-joins-merges-r/
 
 
-part1_join <- merge(FF_0514_1, OLC_updated_1, by.x = "case_code", by.y = "Case #")
-part2_join <- merge(FF_0514_2, OLC_updated_2, by.x = "case_code", by.y = "Case #")
-part3_join <- merge(FF_0514_3, judgments_0514, by = "case_code")
+part1_join <- merge(FF_0522_1, OLC_updated_1, by.x = "case_code", by.y = "Case #")
+part2_join <- merge(FF_0522_2, OLC_updated_2, by.x = "case_code", by.y = "Case #")
+part3_join <- merge(FF_0522_3, judgments_0522, by = "case_code")
 
 # number of rows in part1_join
 nrow(part1_join)
@@ -95,45 +95,59 @@ part1_join_FA_held <- sqldf("select * from part1_join_filter
 nrow(part1_join_FA_held)
 
 # filter through correct eviction rulings
-part1_join_evict_match <- sqldf("select * from part1_join_filter 
+part1_join_evict_match <- sqldf("select * from part1_join_FA_held 
                                                 where 
                                                 [Judgment_General] = '1'
                                                 and
-                                                [Outcome3] = 'Tenant Default' 
+                                                ([Outcome3] = 'Tenant Default' 
                                                 or 
                                                 [Outcome3] = 'Judgment for Landlord' 
                                                 or 
-                                                [Outcome3] = 'Stipulated Agreement' and [SA_noncomp] = '1' 
+                                                [Outcome3] = 'Stipulated Agreement' and [SA_noncomp] = '1')
                                 ")
 nrow(part1_join_evict_match)
-# fitter through eviction rulings per FF but not OLC
-part1_join_evict_not_match_1 <- sqldf("select * from part1_join_filter 
+# filter through eviction rulings per FF but not OLC
+part1_join_evict_not_match_1 <- sqldf("select * from part1_join_FA_held 
                                                 where 
                                                 [Judgment_General] = '1'
-                                                and
-                                                [Outcome3] <> 'Tenant Default' 
-                                                and 
-                                                [Outcome3] <> 'Judgment for Landlord' 
                                                 and not
-                                                [Outcome3] = 'Stipulated Agreement' and [SA_noncomp] = '1' 
+                                                ([Outcome3] = 'Tenant Default' 
+                                                or 
+                                                [Outcome3] = 'Judgment for Landlord' 
+                                                or
+                                                [Outcome3] = 'Stipulated Agreement' and [SA_noncomp] = '1') 
                                 ")
 nrow(part1_join_evict_not_match_1)
-# fitter through eviction rulings per OLC but not FF
-part1_join_evict_not_match_2 <- sqldf("select * from part1_join_filter 
+# filter through eviction rulings per OLC but not FF
+part1_join_evict_not_match_2 <- sqldf("select * from part1_join_FA_held 
                                                 where 
-                                                [Judgment_General] <> '1'
+                                                ([Judgment_General] <> '1' or [Judgment_General] is null)
                                                 and
-                                                [Outcome3] = 'Tenant Default' 
+                                                ([Outcome3] = 'Tenant Default' 
                                                 or 
                                                 [Outcome3] = 'Judgment for Landlord' 
                                                 or 
-                                                [Outcome3] = 'Stipulated Agreement' and [SA_noncomp] = '1' 
+                                                [Outcome3] = 'Stipulated Agreement' and [SA_noncomp] = '1')
                                 ")
 nrow(part1_join_evict_not_match_2)
 
+# filter through OLC eviction rulings
+part1_join_evict_all <- sqldf("select * from part1_join_FA_held 
+                                                where 
+                                                ([Outcome3] = 'Tenant Default' 
+                                                or 
+                                                [Outcome3] = 'Judgment for Landlord' 
+                                                or 
+                                                [Outcome3] = 'Stipulated Agreement' and [SA_noncomp] = '1')
+                                                or
+                                                [Judgment_General] = '1'
+                                                
+                                ")
+nrow(part1_join_evict_all)
+
 
 # filter through correct dismissal rulings
-part1_join_dismiss_match <- sqldf("select * from part1_join_filter 
+part1_join_dismiss_match <- sqldf("select * from part1_join_FA_held 
                                                pgAdmin where 
                                                 [Judgment_Dismissal] = '1'
                                                 and
@@ -141,24 +155,24 @@ part1_join_dismiss_match <- sqldf("select * from part1_join_filter
                                 ")
 nrow(part1_join_dismiss_match)
 # filter through dismissal rulings per FF but not OLC
-part1_join_dismiss_not_match_1 <- sqldf("select * from part1_join_filter 
+part1_join_dismiss_not_match_1 <- sqldf("select * from part1_join_FA_held
                                                 where 
                                                 [Judgment_Dismissal] = '1'
                                                 and
-                                                [Outcome3] <> 'Dismissed' 
+                                                ([Outcome3] <> 'Dismissed' or [Outcome3] is null)
                                 ")
 nrow(part1_join_dismiss_not_match_1)
 # filter through dismissal rulings per OLC but not FF
-part1_join_dismiss_not_match_2 <- sqldf("select * from part1_join_filter 
+part1_join_dismiss_not_match_2 <- sqldf("select * from part1_join_FA_held 
                                                 where 
-                                                [Judgment_Dismissal] <> '1'
+                                                ([Judgment_Dismissal] <> '1' or [Judgment_Dismissal] is null)
                                                 and
                                                 [Outcome3] = 'Dismissed' 
                                 ")
 nrow(part1_join_dismiss_not_match_2)
 
 # filter through correct FTA rulings
-part1_join_FTA_match <- sqldf("select * from part1_join_filter 
+part1_join_FTA_match <- sqldf("select * from part1_join_FA_held 
                                                 where 
                                                 [FTADefault] = '1'
                                                 and
@@ -166,24 +180,24 @@ part1_join_FTA_match <- sqldf("select * from part1_join_filter
                                 ")
 nrow(part1_join_FTA_match)
 # filter through FTA rulings per FF but not OLC
-part1_join_FTA_not_match_1 <- sqldf("select * from part1_join_filter 
+part1_join_FTA_not_match_1 <- sqldf("select * from part1_join_FA_held 
                                                 where 
                                                 [FTADefault] = '1'
                                                 and
-                                                [Outcome3] <> 'Tenant Default' 
+                                                ([Outcome3] <> 'Tenant Default' or [Outcome3] is null) 
                                 ")
 nrow(part1_join_FTA_not_match_1)
 # filter through FTA rulings per OLC but not FF
-part1_join_FTA_not_match_2 <- sqldf("select * from part1_join_filter 
+part1_join_FTA_not_match_2 <- sqldf("select * from part1_join_FA_held 
                                                 where 
-                                                [FTADefault] <> '1'
+                                                ([FTADefault] <> '1' or [FTADefault] is null)
                                                 and
                                                 [Outcome3] = 'Tenant Default' 
                                 ")
 nrow(part1_join_FTA_not_match_2)
 
 # filter through correct FTA rulings with FTA
-part1_join_FTA_match0 <- sqldf("select * from part1_join_filter 
+part1_join_FTA_match0 <- sqldf("select * from part1_join_FA_held 
                                                 where 
                                                 [FTA] = '1'
                                                 and
@@ -191,17 +205,17 @@ part1_join_FTA_match0 <- sqldf("select * from part1_join_filter
                                 ")
 nrow(part1_join_FTA_match0)
 # filter through FTA rulings per FF but not OLC
-part1_join_FTA_not_match0_1 <- sqldf("select * from part1_join_filter 
+part1_join_FTA_not_match0_1 <- sqldf("select * from part1_join_FA_held 
                                                 where 
                                                 [FTA] = '1'
                                                 and
-                                                [Outcome3] <> 'Tenant Default' 
+                                                ([Outcome3] <> 'Tenant Default' or [Outcome3] is null) 
                                 ")
 nrow(part1_join_FTA_not_match0_1)
 # filter through FTA rulings per OLC but not FF
-part1_join_FTA_not_match0_2 <- sqldf("select * from part1_join_filter 
+part1_join_FTA_not_match0_2 <- sqldf("select * from part1_join_FA_held
                                                 where 
-                                                [FTA] <> '1'
+                                                ([FTA] <> '1' or [FTA] is null)
                                                 and
                                                 [Outcome3] = 'Tenant Default' 
                                 ")
@@ -239,10 +253,10 @@ nrow(part2_join_mismatch_tnt)
 
 
 # determine how many eviction rulings occurred this week in each county
-# for this we merge the judgments_0514_curr dataframe with the part1_join_evict_match dataframe (confirmed evictions)
-this_week_evict_1 <- merge(judgments_0514_curr, part1_join_evict_match, by.x = "case_code", by.y = "case_code")
-this_week_evict_2 <- merge(judgments_0514_curr, part1_join_evict_not_match_1, by.x = "case_code", by.y = "case_code")
-this_week_evict_3 <- merge(judgments_0514_curr, part1_join_evict_not_match_2, by.x = "case_code", by.y = "case_code")
+# for this we merge the judgments_0522_curr dataframe with the part1_join_evict_match dataframe (confirmed evictions)
+this_week_evict_1 <- merge(judgments_0522_curr, part1_join_evict_match, by.x = "case_code", by.y = "case_code")
+this_week_evict_2 <- merge(judgments_0522_curr, part1_join_evict_not_match_1, by.x = "case_code", by.y = "case_code")
+this_week_evict_3 <- merge(judgments_0522_curr, part1_join_evict_not_match_2, by.x = "case_code", by.y = "case_code")
 this_week_evict_all <- rbind(this_week_evict_1, this_week_evict_2, this_week_evict_3)
 
 # generate list of how many values are in each this_week_evict table
@@ -275,12 +289,15 @@ for (i in 1:nrow(CountyWeekEvict_curr)) {
 }
 CountyWeekEvict_curr$Count[nrow(CountyWeekEvict_curr)] <- sum(temp1$Freq)
 CountyWeekEvict_curr$Count_full[nrow(CountyWeekEvict_curr)] <- sum(temp2$Freq)
-write.table(CountyWeekEvict_curr, file="G:/Shared drives/ojdevictions/FF_vs_OLC_accuracy/CountyWeeklyEvict_0514.csv", row.names=F, sep=",")
+write.table(CountyWeekEvict_curr, file="G:/Shared drives/ojdevictions/FF_vs_OLC_accuracy/CountyWeeklyEvict_0522.csv", row.names=F, sep=",")
 
 
 #write.table(CountyWeekEvict_base, file="G:/Shared drives/ojdevictions/FF_vs_OLC_accuracy/CountyWeeklyEvict_template.csv", row.names=F, sep=",")
 
+write.table(part1_join_evict_not_match_1, file="G:/Shared drives/ojdevictions/FF_vs_OLC_accuracy/0522_eviction_false_pos.csv", row.names=F, sep=",")
+write.table(part1_join_evict_not_match_2, file="G:/Shared drives/ojdevictions/FF_vs_OLC_accuracy/0522_eviction_false_neg.csv", row.names=F, sep=",")
+write.table(part1_join_dismiss_not_match_1, file="G:/Shared drives/ojdevictions/FF_vs_OLC_accuracy/0522_dismissal_false_pos.csv", row.names=F, sep=",")
+write.table(part1_join_dismiss_not_match_2, file="G:/Shared drives/ojdevictions/FF_vs_OLC_accuracy/0522_dismissal_false_neg.csv", row.names=F, sep=",")
 
-write.table(part1_join_evict_not_match_2, file="G:/Shared drives/ojdevictions/FF_vs_OLC_accuracy/0514_eviction_false_neg.csv", row.names=F, sep=",")
-write.table(part1_join_dismiss_not_match_1, file="G:/Shared drives/ojdevictions/FF_vs_OLC_accuracy/0514_dismissal_false_pos.csv", row.names=F, sep=",")
-write.table(part1_join_dismiss_not_match_2, file="G:/Shared drives/ojdevictions/FF_vs_OLC_accuracy/0514_dismissal_false_neg.csv", row.names=F, sep=",")
+table(this_week_evict_1$location)
+table(this_week_evict_all$location)
